a70568439ed07a9bc585b0e8c70a09f5783af886
lloveg <2337801846@qq.com>
2023-02-17 11:28:01 +0800

    city页面初始化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <router-view />
-  <tab-bar />
+  <tab-bar v-if="!route.meta.hideTabBar" />
 </template>
 
 <script setup>
 import TabBar from "@/components/tab-bar/tab-bar.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute()
 </script>
 
 <style scoped></style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
     <home-nav-bar />
+    <div class="banner">
+      <img src="@/assets/img/home/banner.webp" alt="" />
+    </div>
+    <home-search-box />
   </div>
 </template>
 
 <script setup>
-import HomeNavBar from "@/views/home/components/home-nav-bar.vue"
+import HomeNavBar from "@/views/home/components/home-nav-bar.vue";
+import HomeSearchBox from "@/views/home/components/home-search-box.vue";
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.banner {
+  img {
+    width: 100%;
+  }
+}
+</style>
