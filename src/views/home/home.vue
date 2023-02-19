83b284fa30600a4701d0e37af18b36fceb799adf
lloveg <2337801846@qq.com>
2023-02-19 21:43:29 +0800

    首页热门精选基本完成

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,20 +5,33 @@
       <img src="@/assets/img/home/banner.webp" alt="" />
     </div>
     <home-search-box :hot-suggests="hotSuggests" />
+    <home-categories />
+    <home-content />
+
+    <button @click="btnClick">按钮</button>
   </div>
 </template>
 
 <script setup>
+import { storeToRefs } from "pinia";
+import useHomeStore from "@/stores/modules/home"
+
 import HomeNavBar from "@/views/home/components/home-nav-bar.vue";
 import HomeSearchBox from "@/views/home/components/home-search-box.vue";
-import useHomeStore from "@/stores/modules/home"
-import { storeToRefs } from "pinia";
+import HomeCategories from "@/views/home/components/home-categories.vue";
+import HomeContent from "@/views/home/components/home-content.vue";
 
 // 发送网络请求
 const homeStore = useHomeStore();
 homeStore.fetchHotSuggestData();
+homeStore.fetchCategoriesData();
+homeStore.fetchHouselistData();
 // 1.热门建议
 const { hotSuggests } = storeToRefs(homeStore);
+
+const btnClick = () => {
+  homeStore.fetchHouselistData();
+}
 </script>
 
 <style lang="less" scoped>
