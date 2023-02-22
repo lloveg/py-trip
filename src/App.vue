3a06f12bc485503f77bc658c82bd39e46f09ecaa
lloveg <2337801846@qq.com>
2023-02-22 12:13:36 +0800

    详情页初始化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <router-view />
   <tab-bar v-if="!route.meta.hideTabBar" />
+  <loading />
 </template>
 
 <script setup>
 import TabBar from "@/components/tab-bar/tab-bar.vue";
+import Loading from "@/components/loading/loading.vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute()
