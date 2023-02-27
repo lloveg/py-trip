48048da20909d0dbdfb16ef9f35620ef6f2ba29c
lloveg <2337801846@qq.com>
2023-02-27 21:14:37 +0800

    消息页开发完成

--- src/views/message/message.vue
+++ src/views/message/message.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="message">
-    <h2>message</h2>
+    <!-- 头部 -->
+    <nav-bar left-text="" title="消息中心"></nav-bar>
+    <!-- 内容 -->
+    <div class="content">
+      <!-- 内容 -->
+      <div v-if="messagelist.length" class="list"></div>
+
+      <div v-else class="no-data">
+        <img src="@/assets/img/message/icon_message.png" alt="" />
+        <div class="text">暂无聊天消息</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 
+import NavBar from "@/components/nav-bar/nav-bar.vue";
+
+const messagelist = ref([]);
 </script>
 
 <style lang="less" scoped>
+.message {
+  height: 100vh;
+  overflow-y: auto;
+  background-color: #f7f8fb;
+  
+  .content {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 20px 20px 55px;
 
+    .no-data {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 100px;
+
+      img {
+        width: 88%;
+      }
+    }
+  }
+}
 </style>
